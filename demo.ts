c81dfa383b3e4271a67fe61d756e18eda59361eb
Brandon <amarok1217@163.com>
2023-01-04 14:12:32 +0800

    fix

--- demo.ts
+++ demo.ts
@@ -1,2 +1,7 @@
 import { formateDate } from "./index";
-console.log(formateDate(new Date(), 'Y年Mon月D日 H时Min分S秒'));
+console.log(formateDate(new Date(1672506061000), 'Y年Mon月D日 H时Min分S秒'));
+console.log(formateDate(new Date(1672506061000), 'Y-Mon-D H:Min:S'));
+
+
+
+
--- index.ts
+++ index.ts
@@ -2,20 +2,25 @@ export const formateDate = (
     time: Date,
     formate: string = 'Y年Mon月D日'
 ): string => {
-    let Y = time.getFullYear();
-    let Mon = time.getMonth() + 1;
-    let D = time.getDate();
-    let H = time.getHours();
-    let Min = time.getMinutes();
-    let S = time.getSeconds();
+    let Y = time.getFullYear().toString();
+    let Mon = (time.getMonth() + 1).toString();
+    let D = time.getDate().toString();
+    let H = time.getHours().toString();
+    let Min = time.getMinutes().toString();
+    let S = time.getSeconds().toString();
 
-    formate = formate.replace('Y', Y.toString())
-    formate = formate.replace('Mon', Mon.toString())
-    formate = formate.replace('D', D.toString())
-    formate = formate.replace('H', H.toString())
-    formate = formate.replace('Min', Min.toString())
-    formate = formate.replace('S', S.toString())
+    Mon = parseInt(Mon) < 10 ? '0' + Mon : Mon
+    D = parseInt(D) < 10 ? '0' + D : D
+    H = parseInt(H) < 10 ? '0' + H : H
+    Min = parseInt(Min) < 10 ? '0' + Min : Min
+    S = parseInt(S) < 10 ? '0' + S : S
+
+    formate = formate.replace('Y', Y)
+    formate = formate.replace('Mon', Mon)
+    formate = formate.replace('D', D)
+    formate = formate.replace('H', H)
+    formate = formate.replace('Min', Min)
+    formate = formate.replace('S', S)
 
     return formate
-
 }
